74720765cfcb3c5f6506f0c634d9380edcd3317a
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2017-10-15 00:57:45 -0400

    simplify test-use-lib
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- test-use-lib.c
+++ test-use-lib.c
@@ -13,15 +13,11 @@
 #include <signal.h>
 #include <errno.h>
 #include <limits.h>
+
 #include "rseq.h"
 #include "cpu-op.h"
 
-static inline pid_t gettid(void)
-{
-	return syscall(__NR_gettid);
-}
-
-static int opt_threads = 200, opt_reps = 5000, opt_disable_mod = 0;
+static int opt_reps = 5000;
 
 struct test_data_entry {
 	uintptr_t count;
@@ -31,90 +27,36 @@ struct inc_test_data {
 	struct test_data_entry c[CPU_SETSIZE];
 };
 
-struct inc_thread_test_data {
-	struct inc_test_data *data;
-	int reps;
-	int reg;
-};
+static struct inc_test_data data;
 
-void *test_percpu_inc_thread(void *arg)
+int main(int argc, char **argv)
 {
-	struct inc_thread_test_data *thread_data = arg;
-	struct inc_test_data *data = thread_data->data;
+	struct rseq_state rseq_state;
+	intptr_t *targetptr, newval;
+	uintptr_t sum;
 	int i;
 
-	if (thread_data->reg && rseq_register_current_thread())
-		abort();
-	for (i = 0; i < thread_data->reps; i++) {
+	printf("counter increment\n");
+	for (i = 0; i < opt_reps; i++) {
 		int cpu;
-
-#ifndef SKIP_FASTPATH
-		struct rseq_state rseq_state;
-		intptr_t *targetptr, newval;
 
 		/* Try fast path. */
 		rseq_state = rseq_start();
 		cpu = rseq_cpu_at_start(rseq_state);
-		newval = (intptr_t)data->c[cpu].count + 1;
-		targetptr = (intptr_t *)&data->c[cpu].count;
-		if (unlikely(!rseq_finish(targetptr, newval, rseq_state)))
-#endif
-		{
+		newval = (intptr_t)data.c[cpu].count + 1;
+		targetptr = (intptr_t *)&data.c[cpu].count;
+		if (unlikely(!rseq_finish(targetptr, newval, rseq_state))) {
 			for (;;) {
 				/* Fallback on cpu_op system call. */
 				int ret;
 
 				cpu = rseq_current_cpu_raw();
-				ret = cpu_op_add(&data->c[cpu].count, 1,
+				ret = cpu_op_add(&data.c[cpu].count, 1,
 					sizeof(intptr_t), cpu);
 				if (likely(!ret))
 					break;
 				assert(ret >= 0 || errno == EAGAIN);
 			}
-		}
-
-#ifndef BENCHMARK
-		if (i != 0 && !(i % (thread_data->reps / 10)))
-			printf("tid %d: count %d\n", (int) gettid(), i);
-#endif
-	}
-	if (rseq_unregister_current_thread())
-		abort();
-	return NULL;
-}
-
-void test_percpu_inc(void)
-{
-	const int num_threads = opt_threads;
-	int i, ret;
-	uintptr_t sum;
-	pthread_t test_threads[num_threads];
-	struct inc_test_data data;
-	struct inc_thread_test_data thread_data[num_threads];
-
-	memset(&data, 0, sizeof(data));
-	for (i = 0; i < num_threads; i++) {
-		thread_data[i].reps = opt_reps;
-		if (opt_disable_mod <= 0 || (i % opt_disable_mod))
-			thread_data[i].reg = 1;
-		else
-			thread_data[i].reg = 0;
-		thread_data[i].data = &data;
-		ret = pthread_create(&test_threads[i], NULL,
-			test_percpu_inc_thread, &thread_data[i]);
-		if (ret) {
-			errno = ret;
-			perror("pthread_create");
-			abort();
-		}
-	}
-
-	for (i = 0; i < num_threads; i++) {
-		pthread_join(test_threads[i], NULL);
-		if (ret) {
-			errno = ret;
-			perror("pthread_join");
-			abort();
 		}
 	}
 
@@ -122,12 +64,7 @@ void test_percpu_inc(void)
 	for (i = 0; i < CPU_SETSIZE; i++)
 		sum += data.c[i].count;
 
-	assert(sum == (uintptr_t)opt_reps * num_threads);
-}
+	assert(sum == (uintptr_t)opt_reps);
 
-int main(int argc, char **argv)
-{
-	printf("counter increment\n");
-	test_percpu_inc();
 	return 0;
 }
