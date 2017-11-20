21c2820bc00e5d97429d668057f06bb1292d8b05
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2017-11-20 16:35:20 -0500

    update rseq progress
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- test-rseq-progress.c
+++ test-rseq-progress.c
@@ -22,17 +22,23 @@ intptr_t v;
 int main(int argc, char **argv)
 {
 	int cpu, ret;
+	uint64_t attempts = 0;
 
-	if (rseq_register_current_thread())
+	if (rseq_register_current_thread()) {
+		perror("rseq register");
 		abort();
+	}
 
 	do {
 		cpu = rseq_cpu_start();
 		ret = rseq_addv(&v, 1, cpu);
+		attempts++;
 	} while (ret);
 
-	if (rseq_unregister_current_thread())
+	if (rseq_unregister_current_thread()) {
+		perror("rseq unregister");
 		abort();
-	printf("total %" PRIdPTR "\n", v);
+	}
+	printf("total %" PRIdPTR ", attempts: %" PRIu64 "\n", v, attempts);
 	return 0;
 }
